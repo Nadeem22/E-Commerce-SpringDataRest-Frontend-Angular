5b9ceaad40a7a06c6f10b674974530fcb68d07aa
Nadeem22 <nadeem2107@gmail.com>
2020-04-02 16:50:41 +0530

    initial commit

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "src/app/services/product.service";
+import { Product } from "src/app/common/product";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list-grid.component.html",
+  styleUrls: ["./product-list.component.css"]
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+  currentCategoryId: number;
+  searchMode: boolean;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+  }
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has("keyword");
+    if (this.searchMode) {
+      this.handleSearchProduct();
+    } else {
+      this.handleListProduct();
+    }
+  }
+  handleSearchProduct() {
+    const theKeyword: string = this.route.snapshot.paramMap.get("keyword");
+    //now search product for the given keyword
+    this.productService.searchProducts(theKeyword).subscribe(data => {
+      this.products = data;
+    });
+  }
+  handleListProduct() {
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has("id");
+    if (hasCategoryId) {
+      //get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get("id");
+    } else {
+      //no category id available .... default to category id 1
+      this.currentCategoryId = 1;
+    }
+    //now get the products for the given category "id"
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe(data => {
+        this.products = data;
+      });
+  }
+}
